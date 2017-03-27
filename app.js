24e0df935f56c453e4e95d29aaf2dc7556baadc0
shyling <i@shyling.com>
2017-03-27 13:52:56 +0800

    Use rxjs

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 var os = require('os');
-var _ = require('lodash');
+var Rx = require('rxjs');
 var pm2 = require('pm2');
 var pmx = require('pmx');
 var request = require('request');
 
 function buildMD(data) {
-  return '# ' + data.process.name + '\n'
-    + '*time*: ' + new Date(+data.at).toString() + '\n'
-    + '**message**: ' + data.data.message + '\n'
-    + '**stack**: ' + data.data.stack + '\n';
+  if (data instanceof Array) {
+    return data.map(buildMD).join('------\n');
+  } else {
+    return '**Name**: ' + data.process.name + '\n'
+      + '**Date**: _' + new Date(+data.at).toLocaleString() + '_\n'
+      + (data.data.stack ? data.data.stack : data.data);
+  }
 }
 
-pmx.initModule({
-}, function (err, conf) {
+pmx.initModule({}, function (err, conf) {
 
-  var listenEvents = [
-    // 'log:err',
-    'process:exception',
-  ];
+  var listenEvents = [];
+
+  if(conf.process){
+    listenEvents.push('process:exception');
+  }
+  if(conf.log){
+    listenEvents.push('log:err');
+  }
 
   pm2.launchBus(function (err, bus) {
     listenEvents.forEach(function (listenEvnet) {
-      bus.on(listenEvnet, _.debounce(function (data) {
-        if (data.process.name !== 'pm2-notify') {
+      Rx.Observable.fromEvent(bus, listenEvnet)
+        .filter(data => data.process.name !== conf.module_name)
+        .bufferTime(conf.interval)
+        .filter(dataArr => dataArr.length !== 0)
+        .subscribe(function (dataArr) {
           request.post(conf.url, {
             form: {
-              payload: buildMD(data),
+              payload: JSON.stringify({ text: buildMD(dataArr) })
             }
           });
-        }
-      }, 150));
+        });
     });
   });
 
